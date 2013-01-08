5437664dd840a183399f9846ef4527113c33fdaf
Ted Natoli <ted.e.natoli@gmail.com>
2013-01-08 00:24:47 -0500

    updated to parse leapfrog site

--- savvy.py
+++ savvy.py
@@ -6,10 +6,12 @@ a script for pulling data from data.medicare.gov
 '''
 
 import urllib2
+from BeautifulSoup import BeautifulSoup as bs
 import json
 
 HOA_URL = 'http://data.medicare.gov/api/views/f24z-mvb9/rows.json'
 HAC_URL = 'http://data.medicare.gov/api/views/qd2y-qcgs/rows.json'
+Leapfrog_URL = 'http://www.hospitalsafetyscore.org/search-result.html?zip_code=&hospital=&city=&state_prov=MA&agree=agree&orderby=score&orderby=name'
 
 def subsetToState(data_lists, state, ind):
 	'''
@@ -22,12 +24,24 @@ def subsetToState(data_lists, state, ind):
 			subset.append(d)
 	return subset
 
+def getLeapfrogData(url):
+	'''
+	get patient safety grade from leapfrog website
+	'''
+	# prevent cookies and redirects from interfering
+	#opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+	#response = opener.open(url)
+	#html = response.read()
+	#soup = bs(html)
+	hosp2score = {}
+	page = urllib2.urlopen(url)
+	soup = bs(page)
+	div_tags = soup.findAll('div', attrs={'class':'left-img'})
+	for d in div_tags:
+		# need to get hospital name somehow
+		src = d.img.attrs[0][1]
+		img_name = os.path.splitext(os.path.basename(src))[0]
+		hosp2score(hosp) = img_name[-1]
+
 if __name__ == '__main__':
-	url = urllib2.urlopen(HAC_URL)
-	j = json.load(url)
-	data = j['data']
-	# types of data include int, list, None, unicode
-	# looks like each table entry is stored as a list
-	ma = subsetToState(data, 'MA', 14)
-	for m in ma:
-		print m[14]
+	getLeapfrogData(Leapfrog_URL)
